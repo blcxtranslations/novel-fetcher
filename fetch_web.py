97e722e56bdcc0b24f24ac5c25ce86413ef55e86
blcxtranslations <blcxtranslations@gmail.com>
2017-02-02 21:05:38 -0500

    Added support for one time fetch of any novel on ww

--- fetch_web.py
+++ fetch_web.py
@@ -5,8 +5,18 @@ from web_gravitytales import web_gravitytales
 from web_wuxiaworld import web_wuxiaworld
 
 
+def ask_for_index(str, end, start=1):
+  selection = -1
+  while selection < start or selection > end:
+    selection = raw_input(str)
+    if len(selection) == 0:
+      return None
+    selection = int(selection)
+  return selection - 1
+
 def fetch():
-  links = []
-  links += web_gravitytales()
-  links += web_wuxiaworld()
-  return links
+  selection = ask_for_index('1 for gravitytales, 2 for wuxiaworld : ', 2)
+  if selection == 1:
+    return web_wuxiaworld()
+  else:
+    return web_gravitytales()
--- web_wuxiaworld.py
+++ web_wuxiaworld.py
@@ -6,9 +6,6 @@ import urllib2
 import re
 
 
-url = 'http://www.wuxiaworld.com/emperor-index/'
-
-
 def get_page(url):
   req = urllib2.Request(url , headers={'User-Agent': 'Magic Browser'})
   return urllib2.urlopen(req).read()
@@ -31,9 +28,58 @@ def sort_links(links):
   new_links.sort(key=lambda x: x[0])
   return new_links
 
-def web_wuxiaworld():
+def stripunicode(text):
+    return ''.join(i for i in text if ord(i)<128).strip()
+
+def find_novels():
+  main_url = 'http://www.wuxiaworld.com/'
+  main_page = get_page(main_url)
+  soup = BeautifulSoup(main_page, 'lxml')
+  items = soup.find('nav').findAll('a')
+  items = filter(lambda item: 'index' in item['href'], items)
+  links = []
+  for item in items:
+    title = str(stripunicode(item.text)).replace(' ()', '')
+    link = item['href']
+    links.append([title, link])
+  links.sort(key=lambda x: x[0])
+  return links
+
+def get_length(url):
+  return 100
+
+def get_chapters(url, lower=None, upper=None):
   links = find_links(url)
   links = sort_links(links)
-  links = filter(lambda link: int(link[0]) > 52, links)
+  if not lower is None:
+    links = filter(lambda link: int(link[0]) >= lower, links)
+  if not upper is None:
+    links = filter(lambda link: int(link[0]) <= upper, links)
   links = [link[1] for link in links]
   return links
+
+def ask_for_index(str, end, start=1):
+  selection = -1
+  while selection < start or selection > end:
+    selection = raw_input(str)
+    if len(selection) == 0:
+      return None
+    selection = int(selection)
+  return selection - 1
+
+def web_wuxiaworld():
+  novels = find_novels()
+  for i, novel in enumerate(novels):
+    print i + 1, '\t:\t', novel[0]
+
+  selection = -1
+  while selection < 1 or selection > len(novels):
+    selection = int(raw_input("Select a novel: "))
+  selection -= 1
+
+  chapters = get_chapters(novels[selection][1])
+  print "There are %s chapters in %s" % (len(chapters), novels[selection][0])
+  lower = ask_for_index("Start from chapter: ", len(chapters))
+  upper = ask_for_index("End at chapter: ", len(chapters))
+  chapters = chapters[lower:upper]
+  return chapters
