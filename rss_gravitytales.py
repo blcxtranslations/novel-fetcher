a031e3f0638ca63ca9d9a3758552213b158184df
blcxtranslations <blcxtranslations@gmail.com>
2017-02-02 21:19:34 -0500

    Completed gt rss scraping

--- rss_gravitytales.py
+++ rss_gravitytales.py
@@ -1,5 +1,50 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from bs4 import BeautifulSoup
+import feedparser
+import re
+import urllib2
+import xml.etree.ElementTree as ET
+
+
+novels = {
+  'single': [
+    "Ancient Godly Monarch",
+    "Chaotic Lightning Cultivation",
+  ],
+  'multi': [
+  ]
+}
+
+
+def stripunicode(text):
+    return ''.join(i for i in text if ord(i)<128).strip()
+
+def get_page(url):
+  req = urllib2.Request(url , headers={'User-Agent': 'Magic Browser'})
+  return urllib2.urlopen(req).read()
+
+def find_links(link_url):
+  soup = BeautifulSoup(get_page(link_url), "lxml")
+  article = soup.find('article')
+  links = [a['href'] for a in article.findAll('a')]
+  links = filter(lambda link: 'gravitytales.com' in link and 'novel' in link, links)
+  links = list(set(links))
+  return links
+
+def parse_feed():
+  releases = []
+  feed = feedparser.parse('http://gravitytales.com/feed')
+  for entry in feed.entries:
+    link = stripunicode(entry.link)
+    links = []
+    if any(novel in entry.title for novel in novels['single']):
+      links = find_links(link)
+    # elif entry.category in novels['multi']:
+      # links = construct_links(link)
+    releases += links
+  return releases
+
 def rss_gravitytales():
-  return []
+  return parse_feed()
